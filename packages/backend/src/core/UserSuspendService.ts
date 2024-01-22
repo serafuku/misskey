944cf7111833090f68ed9cafe4fdeeaae0cd0135
yuno <yuno@yunochi.com>
2024-01-22 23:34:12 +0900

    유저 삭제 액티비티를 알고있는 모든 inbox에 전달하도록

--- packages/backend/src/core/UserSuspendService.ts
+++ packages/backend/src/core/UserSuspendService.ts
@@ -88,18 +88,14 @@ export class UserSuspendService {
 
 			const queue: string[] = [];
 
-			const followings = await this.followingsRepository.find({
-				where: [
-					{ followerSharedInbox: Not(IsNull()) },
-					{ followeeSharedInbox: Not(IsNull()) },
-				],
-				select: ['followerSharedInbox', 'followeeSharedInbox'],
-			});
-
-			const inboxes = followings.map(x => x.followerSharedInbox ?? x.followeeSharedInbox);
+			const inboxes = await this.usersRepository.createQueryBuilder('user')
+				.select('user.sharedInbox')
+				.distinctOn(['user.sharedInbox'])
+				.where('user.sharedInbox IS NOT NULL')
+				.getMany();
 
 			for (const inbox of inboxes) {
-				if (inbox != null && !queue.includes(inbox)) queue.push(inbox);
+				if (inbox.sharedInbox != null) queue.push(inbox.sharedInbox);
 			}
 
 			for (const inbox of queue) {
@@ -118,18 +114,14 @@ export class UserSuspendService {
 
 			const queue: string[] = [];
 
-			const followings = await this.followingsRepository.find({
-				where: [
-					{ followerSharedInbox: Not(IsNull()) },
-					{ followeeSharedInbox: Not(IsNull()) },
-				],
-				select: ['followerSharedInbox', 'followeeSharedInbox'],
-			});
-
-			const inboxes = followings.map(x => x.followerSharedInbox ?? x.followeeSharedInbox);
+			const inboxes = await this.usersRepository.createQueryBuilder('user')
+				.select('user.sharedInbox')
+				.distinctOn(['user.sharedInbox'])
+				.where('user.sharedInbox IS NOT NULL')
+				.getMany();
 
 			for (const inbox of inboxes) {
-				if (inbox != null && !queue.includes(inbox)) queue.push(inbox);
+				if (inbox.sharedInbox != null) queue.push(inbox.sharedInbox);
 			}
 
 			for (const inbox of queue) {
