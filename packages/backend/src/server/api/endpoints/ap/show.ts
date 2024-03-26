70b020cbb6843d4318bdfe4dcc09f5556da1bc21
Yunochi <yuno@yunochi.com>
2024-03-26 19:23:32 +0900

    추가 요청에도 유저의 액터 키로 서명하도록

--- packages/backend/src/server/api/endpoints/ap/show.ts
+++ packages/backend/src/server/api/endpoints/ap/show.ts
@@ -149,7 +149,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 		return await this.mergePack(
 			me,
 			isActor(object) ? await this.apPersonService.createPerson(getApId(object)) : null,
-			isPost(object) ? await this.apNoteService.createNote(getApId(object), undefined, true) : null,
+			isPost(object) ? await this.apNoteService.createNote(getApId(object), resolver, true) : null,
 		);
 	}
 
