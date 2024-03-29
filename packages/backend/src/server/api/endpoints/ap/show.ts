b63e7db2607b17220e590e87d2fd9677ed298ac7
YUNOCHI <yuno@yunochi.com>
2024-03-29 23:51:48 +0900

    resolver 를 createNote에서 재사용하기 전에 history 를 clear하도록 수정

--- packages/backend/src/server/api/endpoints/ap/show.ts
+++ packages/backend/src/server/api/endpoints/ap/show.ts
@@ -19,9 +19,9 @@ import { UserEntityService } from '@/core/entities/UserEntityService.js';
 import { NoteEntityService } from '@/core/entities/NoteEntityService.js';
 import { UtilityService } from '@/core/UtilityService.js';
 import { bindThis } from '@/decorators.js';
-import { ApiError } from '../../error.js';
 import { LoggerService } from '@/core/LoggerService.js';
 import type Logger from '@/logger.js';
+import { ApiError } from '../../error.js';
 
 export const meta = {
 	tags: ['federation'],
@@ -100,7 +100,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 		private apDbResolverService: ApDbResolverService,
 		private apPersonService: ApPersonService,
 		private apNoteService: ApNoteService,
-		private loggerService: LoggerService
+		private loggerService: LoggerService,
 	) {
 		super(meta, paramDef, async (ps, me) => {
 			const object = await this.fetchAny(ps.uri, me);
@@ -130,7 +130,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 
 		// リモートから一旦オブジェクトフェッチ
 		const resolver = this.apResolverService.createResolver();
-		if(me){
+		if (me) {
 			this.logger.info(`Set signed GET User to ${me.username}`);
 			resolver.setSignUser(me);
 		}
@@ -146,6 +146,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> { // eslint-
 			if (local != null) return local;
 		}
 
+		resolver.clearHistory();
 		return await this.mergePack(
 			me,
 			isActor(object) ? await this.apPersonService.createPerson(getApId(object)) : null,
