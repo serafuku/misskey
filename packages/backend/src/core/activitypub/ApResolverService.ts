53430a24af2cf414144c02d7db5d2804d1952c6b
YUNOCHI <yuno@yunochi.com>
2024-03-29 23:51:48 +0900

    resolver 를 createNote에서 재사용하기 전에 history 를 clear하도록 수정

--- packages/backend/src/core/activitypub/ApResolverService.ts
+++ packages/backend/src/core/activitypub/ApResolverService.ts
@@ -53,6 +53,11 @@ export class Resolver {
 	}
 
 	@bindThis
+	public clearHistory() {
+		this.history.clear();
+	}
+
+	@bindThis
 	public getHistory(): string[] {
 		return Array.from(this.history);
 	}
