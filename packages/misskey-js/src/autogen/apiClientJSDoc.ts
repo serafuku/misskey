f66a60e87dfcbb8dac3fc33f3c7bec68b0419acc
Yunochi <yuno@yunochi.com>
2024-08-13 18:59:21 +0900

    read:account 권한으로 token revoke 가능

--- packages/misskey-js/src/autogen/apiClientJSDoc.ts
+++ packages/misskey-js/src/autogen/apiClientJSDoc.ts
@@ -2953,8 +2953,7 @@ declare module '../api.js' {
     /**
      * No description provided.
      * 
-     * **Internal Endpoint**: This endpoint is an API for the misskey mainframe and is not intended for use by third parties.
-     * **Credential required**: *Yes*
+     * **Credential required**: *Yes* / **Permission**: *read:account*
      */
     request<E extends 'i/revoke-token', P extends Endpoints[E]['req']>(
       endpoint: E,
