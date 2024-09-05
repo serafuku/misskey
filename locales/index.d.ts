e21237d167be245d832c79594a9b97dc4f6be3a4
Squarecat-meow <kw7551@gmail.com>
2024-09-05 13:52:56 +0000

    💄

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -5076,6 +5076,14 @@ export interface Locale extends ILocale {
      * 修正履歴
      */
     "editHistory": string;
+    /**
+     * 開く
+     */
+    "fold": string;
+    /**
+     * 閉じる
+     */
+    "unfold": string;
     "_delivery": {
         /**
          * 配信状態
