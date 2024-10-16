0caadf1442e9fe83d9a357fddf89f5b28677da0a
Yunochi <yuno@yunochi.com>
2024-10-16 14:01:51 +0900

    Feat: 용도별 Object스토리지 PATH 분리 기능
    - 외부 인스턴스 미디어 캐시 Path별도 지정 가능하도록
    - Remote 파일 저장할 Prefix 를 meta 에 추가
    - Frontend에 설정 추가
    - original,webpublic,thumbnail 를 별도의 Path에 저장

--- packages/misskey-js/src/autogen/types.ts
+++ packages/misskey-js/src/autogen/types.ts
@@ -9455,6 +9455,7 @@ export interface operations {
                         objectStorageBaseUrl: string | null;
                         objectStorageBucket: string | null;
                         objectStoragePrefix: string | null;
+                        objectStoragePrefixForRemote: string | null;
                         objectStorageEndpoint: string | null;
                         objectStorageRegion: string | null;
                         objectStoragePort: number | null;
@@ -12796,6 +12797,7 @@ export interface operations {
                     objectStorageBaseUrl?: string | null;
                     objectStorageBucket?: string | null;
                     objectStoragePrefix?: string | null;
+                    objectStoragePrefixForRemote?: string | null;
                     objectStorageEndpoint?: string | null;
                     objectStorageRegion?: string | null;
                     objectStoragePort?: number | null;
