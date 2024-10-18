4f0095f92d187cee16ef875f639fda6f39a4d230
Yunochi <yuno@yunochi.com>
2024-10-18 15:09:14 +0900

    Add DB migration

---
+++ packages/backend/migration/1729234755418-DriveFile-CacheExpire.js
@@ -0,0 +1,36 @@
+
+export class DriveFileCacheExpire1729234755418 {
+	name = 'DriveFileCacheExpire1729234755418';
+
+	async up(queryRunner) {
+		await queryRunner.query(`
+            ALTER TABLE "drive_file"
+            ADD "cachedAt" TIMESTAMP WITH TIME ZONE
+        `);
+		await queryRunner.query(`
+            COMMENT ON COLUMN "drive_file"."cachedAt" IS 'Cache Timestamp'
+        `);
+		await queryRunner.query(`
+            ALTER TABLE "meta"
+            ADD "objectStorageCacheDays" integer
+        `);
+		await queryRunner.query(`
+            CREATE INDEX "IDX-drive-cachedAt-mua" ON "drive_file" ("cachedAt")
+        `);
+	}
+
+	async down(queryRunner) {
+		await queryRunner.query(`
+            DROP INDEX "public"."IDX-drive-cachedAt-mua"
+        `);
+		await queryRunner.query(`
+            ALTER TABLE "meta" DROP COLUMN "objectStorageCacheDays"
+        `);
+		await queryRunner.query(`
+            COMMENT ON COLUMN "drive_file"."cachedAt" IS 'Cache Timestamp'
+        `);
+		await queryRunner.query(`
+            ALTER TABLE "drive_file" DROP COLUMN "cachedAt"
+        `);
+	}
+}
